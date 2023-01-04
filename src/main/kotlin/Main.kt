03c85b787f4a510922cda791aac32b0d476176d4
Joshua-Martel <115580065+Joshua-Martel@users.noreply.github.com>
2023-01-04 11:51:23 -0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("This is a Knoldus base asccelerator starting pack. Keep on learning!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
